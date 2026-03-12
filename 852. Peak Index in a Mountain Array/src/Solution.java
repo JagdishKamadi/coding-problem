57b349b093535637e3ecd54897c0b99a1b1ba770
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2026-03-12 21:01:53 +0530

    Updated solution for 852. Peak Index in a Mountain Array problem.

--- 852. Peak Index in a Mountain Array/src/Solution.java
+++ 852. Peak Index in a Mountain Array/src/Solution.java
@@ -2,16 +2,16 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int left = 0;
         int right = arr.length - 1;
-        while (left <= right) {
+
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
-                return mid;
-            } else if (arr[mid] > arr[mid - 1]) {
-                left = mid + 1;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;   // move to increasing side
             } else {
-                right = mid - 1;
+                right = mid;      // peak is at mid or left side
             }
         }
-        return -1;
+        return left;
     }
 }
