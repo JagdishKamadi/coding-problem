a6dd9bf05187c1c716e3c72329f55f978849c427
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2026-04-24 08:15:10 +0530

    Added solution for 236. Lowest Common Ancestor of a Binary Tree coding problem.

---
+++ 236. Lowest Common Ancestor of a Binary Tree/src/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return find(root, p, q);
+    }
+
+    private TreeNode find(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode leftLCA = find(root.left, p, q);
+        TreeNode rightLCA = find(root.right, p, q);
+
+        if (leftLCA != null && rightLCA != null) {
+            return root;
+        } else if (leftLCA != null) {
+            return leftLCA;
+        } else {
+            return rightLCA;
+        }
+    }
+}
