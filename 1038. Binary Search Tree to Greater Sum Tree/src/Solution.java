afbda7a1ce2f444a22ebdcbecdc178a6448d2a52
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2026-04-27 20:52:58 +0530

    Added solution for 1038. Binary Search Tree to Greater Sum Tree coding problem.

---
+++ 1038. Binary Search Tree to Greater Sum Tree/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        helper(root);
+        return root;
+    }
+
+    private void helper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper(root.right);
+        sum += root.val;
+        root.val = sum;
+        helper(root.left);
+    }
+}
