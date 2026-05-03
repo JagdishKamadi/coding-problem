699571f67ed0ff9f35452ae696fe3de04f83ccd4
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2026-05-03 15:39:53 +0530

    Added solution for 173. Binary Search Tree Iterator coding problem.

--- 173. Binary Search Tree Iterator/src/BSTIterator2.java
+++ 173. Binary Search Tree Iterator/src/BSTIterator2.java
@@ -1,27 +1,28 @@
 import java.util.Stack;
 
 public class BSTIterator2 {
-    private Stack<Integer> stack = new Stack<>();
+    private Stack<TreeNode> stack = new Stack<>();
 
     public BSTIterator2(TreeNode root) {
-        inorderTraversal(root);
+        pushLeft(root);
     }
 
     public int next() {
-        return stack.pop();
+        TreeNode node = stack.pop();
+        if (node.right != null) {
+            pushLeft(node.right);
+        }
+        return node.val;
     }
 
     public boolean hasNext() {
         return !stack.isEmpty();
     }
 
-    private void inorderTraversal(TreeNode root) {
-        if (root == null) {
-            return;
+    private void pushLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
         }
-        // Reverse inorder: Right → Root → Left
-        inorderTraversal(root.right);
-        stack.push(root.val);
-        inorderTraversal(root.left);
     }
 }
