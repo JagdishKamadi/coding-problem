b09a1509b238555f3da1fefe1b42639f47ba713c
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2026-07-31 22:13:15 +0530

    Updated solution for 31. Next Permutation problem.

--- 31. Next Permutation/src/Solution.java
+++ 31. Next Permutation/src/Solution.java
@@ -6,25 +6,26 @@ public class Solution {
         if (n == 1) {
             return;
         }
-        int i = 1;
         int lastIndex = -1;
+        int i = 1;
         while (i < n) {
-            if (arr[i] > arr[i - 1]) {
+            if (arr[i - 1] < arr[i]) {
                 lastIndex = i;
             }
             i++;
         }
-        System.out.println(lastIndex);
-        // that means array is sorted, we have to reverse the array now
+
+        // lastIndex=-1, means There is no increasing pair.
         if (lastIndex == -1) {
-            int left = 0, right = n - 1;
+            int left = 0;
+            int right = n - 1;
             while (left <= right) {
                 swap(arr, left++, right--);
             }
             return;
         }
-        int index = lastIndex;
 
+        int index = lastIndex;
         for (i = lastIndex; i < n; i++) {
             if (arr[i] > arr[lastIndex - 1] && arr[i] < arr[index]) {
                 index = i;
@@ -34,14 +35,14 @@ public class Solution {
         Arrays.sort(arr, lastIndex, n);
     }
 
-    private static void swap(int[] arr, int left, int right) {
+    private void swap(int[] arr, int left, int right) {
         int temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,3,5,4,2}; // 14235
+        int[] arr = {1, 3, 5, 4, 2}; // 14235
         new Solution().nextPermutation(arr);
         System.out.println(Arrays.toString(arr));
     }
