beca8f61997ae629ebdbaf8f06257c708466f004
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2026-08-02 09:12:47 +0530

    Updated the solution for 79. Word Search problem.

--- 79. Word Search/src/Solution.java
+++ 79. Word Search/src/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        char[] wordCharArray = word.toCharArray();
+        char[] wordArray = word.toCharArray();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0) || exists(board, i, j, wordCharArray, 0)) {
+                if (board[i][j] == wordArray[0] && exist(board, i, j, wordArray, 0)) {
                     return true;
                 }
             }
@@ -11,21 +11,21 @@ class Solution {
         return false;
     }
 
-    private boolean exists(char[][] board, int i, int j, char[] word, int index) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word[index] || board[i][j] == '*') {
+    private boolean exist(char[][] board, int i, int j, char[] wordArray, int index) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != wordArray[index] || board[i][j] == '*') {
             return false;
         }
-        if (index == word.length - 1) {
+        if (wordArray.length - 1 == index) {
             return true;
         }
-        char c = board[i][j];
+        char ch = board[i][j];
         board[i][j] = '*';
-        boolean result =
-                exists(board, i + 1, j, word, index + 1) ||
-                        exists(board, i - 1, j, word, index + 1) ||
-                        exists(board, i, j + 1, word, index + 1) ||
-                        exists(board, i, j - 1, word, index + 1);
-        board[i][j] = c;
+        boolean result = exist(board, i + 1, j, wordArray, index + 1)
+                || exist(board, i - 1, j, wordArray, index + 1)
+                || exist(board, i, j + 1, wordArray, index + 1)
+                || exist(board, i, j - 1, wordArray, index + 1);
+        board[i][j] = ch;
+
         return result;
     }
 
