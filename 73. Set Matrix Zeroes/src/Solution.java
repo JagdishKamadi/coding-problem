dd23cda1c04dfd83183ebf7b4a013bff70f2c434
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2026-08-02 08:38:50 +0530

    Updated the solution for 73. Set Matrix Zeroes problem.

--- 73. Set Matrix Zeroes/src/Solution.java
+++ 73. Set Matrix Zeroes/src/Solution.java
@@ -1,12 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        System.out.println(m);
-        System.out.println(n);
-        boolean firstRow = false;
         boolean firstColumn = false;
-        // set markers in first row and column
+        boolean firstRow = false;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -16,12 +16,12 @@ class Solution {
                     if (j == 0) {
                         firstColumn = true;
                     }
-                    matrix[0][j] = 0;
                     matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        // traverse the inner matrix
+
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -30,23 +30,23 @@ class Solution {
             }
         }
 
-        // last remaining checks
         if (firstRow) {
-            for (int i = 0; i < n; i++) {
+            for (int i = 1; i < n; i++) {
                 matrix[0][i] = 0;
             }
         }
-
         if (firstColumn) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
+            for (int j = 1; j < m; j++) {
+                matrix[j][0] = 0;
             }
         }
-
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        int[][] matrix = {{1, 0, 1}, {1, 1, 1}, {0, 1, 1}};
         new Solution().setZeroes(matrix);
+        for (int[] arr : matrix) {
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
